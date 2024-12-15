d7f004b6264cbfb73a3785001ef4e57c7dcbe6c9
vctoraguilar <vicaguilarpi@upt.pe>
2024-12-14 22:40:57 -0500

    SONAR

--- UITests/obj/Debug/net8.0/UITests.AssemblyInfo.cs
+++ UITests/obj/Debug/net8.0/UITests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UITests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99d197595d7059280b4c239211cc73d0eeaed328")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08c343913bdfeb2615a87f250381243f99aee342")]
 [assembly: System.Reflection.AssemblyProductAttribute("UITests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UITests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
